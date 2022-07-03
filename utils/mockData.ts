ec834bd8ce99b6b564a02173b24d62bdcb50392c
Henry <henrykc24@gmail.com>
2022-07-03 14:30:58 +0100

    All mock data

---
+++ utils/mockData.ts
@@ -0,0 +1,36 @@
+const mockData = [
+  {
+    id: '1',
+    salary: 15000,
+    documents: [
+      { id: '1', name: 'Offer.pdf' },
+      { id: '2', name: 'Employment.pdf' },
+      { id: '3', name: 'Assignment.pdf' }
+    ]
+  },
+  {
+    id: '2',
+    salary: 15000,
+    documents: [
+      { id: '1', name: 'Offer.pdf' },
+      { id: '2', name: 'Employment.pdf' },
+      { id: '3', name: 'Offer.pdf' },
+      { id: '4', name: 'Employment.pdf' },
+      { id: '5', name: 'Offer.pdf' },
+      { id: '6', name: 'Communication_line.pdf' },
+      { id: '7', name: 'Offer.pdf' },
+      { id: '8', name: 'Assignment.pdf' }
+    ]
+  },
+  {
+    id: '3',
+    salary: 15000,
+    documents: [
+      { id: '1', name: 'Offer.pdf' },
+      { id: '2', name: 'Employment.pdf' },
+      { id: '3', name: 'Assignment.pdf' }
+    ]
+  }
+]
+
+export default mockData;
